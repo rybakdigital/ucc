4e69022b78c1badbe821832a878e8fa728314cba
krisrybak <kris.rybak@hotmail.co.uk>
2014-12-15 11:04:03 +0000

    Adds methods to setup application configuration

--- Fundation/Config.php
+++ Fundation/Config.php
@@ -2,6 +2,54 @@
 
 namespace Ucc\Fundation;
 
+/**
+ * This class provides utility methods for reading and storing configuration.
+ *
+ * @author Kris Rybak <kris@krisrybak.com>
+ */
 class Config
 {
+    /**
+     * @var ParameterBagInterface;
+     */
+    private $parameterBag;
+
+    /**
+     * Gets the parameter bag.
+     */
+    public function getParameterBag()
+    {
+        return $this->parameterBag;
+    }
+
+    /**
+     * Gets a parameter.
+     *
+     * @param string    $name   Name of the parameter
+     */
+    public function getParameter($name)
+    {
+        return $this->parameterBag->get($name);
+    }
+
+    /**
+     * Checks if parameter exists.
+     *
+     * @param string    $name   Name of the parameter
+     */
+    public function hasParameter($name)
+    {
+        return $this->parameterBag->has($name);
+    }
+
+    /**
+     * Sets a parameter.
+     *
+     * @param string $name  The parameter name
+     * @param mixed  $value The parameter value
+     */
+    public function setParameter($name, $value)
+    {
+        $this->parameterBag->set($name, $value);
+    }
 }
