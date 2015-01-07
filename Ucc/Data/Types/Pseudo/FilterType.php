67b5f273be72158464ec7b1400f85e0c9344fc23
krisrybak <kris.rybak@hotmail.co.uk>
2015-01-07 13:36:14 +0000

    Adds FilterType

---
+++ Ucc/Data/Types/Pseudo/FilterType.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Ucc\Data\Types\Pseudo;
+
+use Ucc\Data\Types\TypeInterface;
+use Ucc\Exception\Data\InvalidDataTypeException;
+use Ucc\Exception\Data\InvalidDataValueException;
+use Ucc\Exception\Data\InvalidDataException;
+
+/**
+ * Ucc\Data\Types\Pseudo\FilterType
+ * Defines FilterType data
+ *
+ * @author Kris Rybak <kris@krisrybak.com>
+ */
+class FilterType implements TypeInterface
+{
+}
