de8bfc44a4606f8555faa06c3da6e4a0184503ca
krisrybak <kris.rybak@hotmail.co.uk>
2015-01-07 09:10:04 +0000

    Adds file for string type filter

---
+++ Ucc/Data/Types/Basic/StringType.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ucc\Data\Types\Basic;
+
+use Ucc\Data\Types\TypeInterface;
+use Ucc\Exception\InvalidDataTypeException;
+
+/**
+ * Ucc\Data\Types\Basic\StringType
+ * Defines IntegerType data
+ *
+ * @author Kris Rybak <kris@krisrybak.com>
+ */
+class StringType implements TypeInterface
+{
+}
