95c251cd6bc31cde4ec10d17260de46faabadfbf
krisrybak <kris.rybak@hotmail.co.uk>
2015-01-07 13:32:33 +0000

    Adds PseudoTypes

---
+++ Ucc/Data/Types/Pseudo/PseudoTypes.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ucc\Data\Types\Pseudo;
+
+/**
+ * Ucc\Data\Types\Pseudo\BasicTypes
+ * Defines checks for basic types
+ *
+ * @author Kris Rybak <kris@krisrybak.com>
+ */
+class PseudoTypes
+{
+}
