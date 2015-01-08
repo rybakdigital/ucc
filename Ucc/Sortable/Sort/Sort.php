101bdc9a8e2d8f1f12668ab39703355bf9a768fd
krisrybak <kris.rybak@hotmail.co.uk>
2015-01-08 15:50:51 +0000

    Moves files to Sortable and adds base class definitions

---
+++ Ucc/Sortable/Sort/Sort.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Ucc\Sortable\Sort;
+
+use \InvalidArgumentException;
+use Ucc\Sortable\Sort\SortInterface;
+
+/**
+ * Ucc\Sortable\Sort\Sort
+ * Allows to represent sort directive in sting logic format
+ *
+ * @author Kris Rybak <kris@krisrybak.com>
+ */
+class Sort implements SortInterface
+{
+
+}
---
+++ Ucc/Sortable/Sort/SortInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Ucc\Sortable\Sort;
+
+/**
+ * Ucc\Sortable\Sort\SortInterface.
+ *
+ * @author Kris Rybak <kris@krisrybak.com>
+ */
+interface SortInterface
+{
+}
