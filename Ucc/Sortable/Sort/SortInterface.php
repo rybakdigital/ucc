3493d3056749518ef3c04695834fa24bfd0e7cd0
krisrybak <kris.rybak@hotmail.co.uk>
2015-01-08 15:54:48 +0000

    Defines SortInterface

--- Ucc/Sortable/Sort/SortInterface.php
+++ Ucc/Sortable/Sort/SortInterface.php
@@ -9,4 +9,49 @@ namespace Ucc\Sortable\Sort;
  */
 interface SortInterface
 {
+    /**
+     * Gets field.
+     *
+     * @return string
+     */
+    public function getField();
+
+    /**
+     * Alias of getField().
+     *
+     * @return string
+     */
+    public function field();
+
+    /**
+     * Sets field.
+     *
+     * @param   string  $field
+     * @return  Ucc\Sortable\Sort\Sort
+     * @throws  InvalidArgumentException
+     */
+    public function setField($field);
+
+    /**
+     * Gets direction.
+     *
+     * @return string
+     */
+    public function getDirection();
+
+    /**
+     * Alias of getDirection().
+     *
+     * @return string
+     */
+    public function direction();
+
+    /**
+     * Sets direction.
+     *
+     * @param   string  $direction
+     * @return  Ucc\Sortable\Sort\Sort
+     * @throws  InvalidArgumentException
+     */
+    public function setDirection($direction);
 }
