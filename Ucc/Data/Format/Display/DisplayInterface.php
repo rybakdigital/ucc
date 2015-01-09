84ae20cc75562762cb6bfa780136e59324c386c8
krisrybak <kris.rybak@hotmail.co.uk>
2015-01-09 16:04:18 +0000

    Adds Display and DisplayInterface

---
+++ Ucc/Data/Format/Display/DisplayInterface.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Ucc\Data\Format\Display;
+
+/**
+ * Ucc\Data\Format\Display\DisplayInterface
+ *
+ * @author Kris Rybak <kris@krisrybak.com>
+ */
+interface DisplayInterface
+{
+    /**
+     * Gets field.
+     *
+     * @return string
+     */
+    public function getField();
+
+    /**
+     * Alias of getField().
+     *
+     * @return string
+     */
+    public function field();
+
+    /**
+     * Sets field.
+     *
+     * @param   string  $field
+     * @return  Ucc\Data\Format\Display\Display
+     * @throws  InvalidArgumentException
+     */
+    public function setField($field);
+
+    /**
+     * Gets alias.
+     *
+     * @return string
+     */
+    public function getAlias();
+
+    /**
+     * Alias of getAlias().
+     *
+     * @return string
+     */
+    public function alias();
+
+    /**
+     * Sets alias.
+     *
+     * @param   string  $alias
+     * @return  Ucc\Data\Format\Display\Display
+     * @throws  InvalidArgumentException
+     */
+    public function setAlias($alias);
+
+    /**
+     * Turns Display into string in the following format: {field}-{alias}
+     *
+     * @return  Ucc\Data\Format\Display\Display
+     */
+    public function toString();
+}
