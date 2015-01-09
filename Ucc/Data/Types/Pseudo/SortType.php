4a30e9cc4c7685158a8ebbc37c0ded9308f0433c
krisrybak <kris.rybak@hotmail.co.uk>
2015-01-09 11:25:33 +0000

    Adds base for SortType class

---
+++ Ucc/Data/Types/Pseudo/SortType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Ucc\Data\Types\Pseudo;
+
+use Ucc\Data\Types\TypeInterface;
+use Ucc\Filter\Sortable\Sort;
+use Ucc\Exception\Data\InvalidDataTypeException;
+use Ucc\Exception\Data\InvalidDataValueException;
+use Ucc\Exception\Data\InvalidDataException;
+use \InvalidArgumentException;
+
+/**
+ * Ucc\Data\Types\Pseudo\SortType
+ * Defines SortType data
+ *
+ * @author Kris Rybak <kris@krisrybak.com>
+ */
+class SortType implements TypeInterface
+{
+}
