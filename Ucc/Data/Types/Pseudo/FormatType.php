69a6a1d3ca87f528fec86d61fcaa6b7d4fbff425
krisrybak <kris.rybak@hotmail.co.uk>
2015-01-13 14:15:36 +0000

    Defines class basics

---
+++ Ucc/Data/Types/Pseudo/FormatType.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Ucc\Data\Types\Pseudo;
+
+use Ucc\Data\Types\TypeInterface;
+use Ucc\Data\Format\Format\Format;
+use Ucc\Exception\Data\InvalidDataTypeException;
+use Ucc\Exception\Data\InvalidDataValueException;
+use Ucc\Exception\Data\InvalidDataException;
+use \InvalidArgumentException;
+
+/**
+ * Ucc\Data\Types\Pseudo\FormatType
+ * Defines FormatType data
+ *
+ * @author Kris Rybak <kris@krisrybak.com>
+ */
+class FormatType implements TypeInterface
+{
+}
