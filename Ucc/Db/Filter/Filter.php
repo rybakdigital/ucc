3d22b031abc0dbde85ab09b62098c6a98ed2b020
krisrybak <kris.rybak@hotmail.co.uk>
2015-04-16 16:00:48 +0100

    Updates Filter class with criterion methods

--- Ucc/Db/Filter/Filter.php
+++ Ucc/Db/Filter/Filter.php
@@ -3,8 +3,10 @@
 namespace Ucc\Db\Filter;
 
 use Ucc\Exception\Data\InvalidDataTypeException;
+use Ucc\Exception\Data\InvalidDataException\InvalidDataValueException;
 use Ucc\Data\Types\Pseudo\FilterType;
 use Ucc\Data\Filter\Criterion\Criterion;
+use Ucc\Db\Filter\DQL;
 
 /**
  * Ucc\Db\Filter\Filter
@@ -38,73 +40,73 @@ class Filter
 
     public static function criterionToDQL(Criterion $criterion)
     {
-            switch ($criterion->op())
-            {
-                // Boolean check to see if the value is set or not.
-                case 'bool':
-                    $method = 'criterionToDQLBool';
-                    break;
+        $method = self::criterionOperandToMethod($criterion);
 
-                // Direct comparison checks.
-                case 'eq': // equals
-                case 'ne': // does not equal
-                case 'eqi': // equals (case insensitive)
-                case 'nei': // does not equal (case insensitive)
-                    $method = 'criterionToDQLEq';
-                    break;
+        DQL::$method($criterion);
+                // // Methods modify the $clause object so no return value required.
+                // // Note no default method - keep default return values if the
+                // // method is invalid or unrecognised.
+                // if ( method_exists( 'Ucc\Db\Filter\Filter', $method ) )
+                // {
+                //     Ucc\Db\Filter\Filter::$method;
+                // }
+    }
 
-                // Relative comparison checks.
-                case 'gt': // greater than
-                case 'ge': // greater than or equal to
-                case 'lt': // less than
-                case 'le': // less than or equal to
-                    $method = 'criterionToDQLGt';
-                    break;
+    public static function criterionOperandToMethod(Criterion $criterion)
+    {
+        switch ($criterion->op())
+        {
+            // Boolean check to see if the value is set or not.
+            case 'bool':
+                $method = 'criterionToBool';
+                break;
 
-                // Percentage comparison checks.
-                case 'gtp': // greater than % of
-                case 'gep': // greater than or equal to % of
-                case 'ltp': // less than % of
-                case 'lep': // less than or equal to % of
-                    $method = 'criterionToDQLGtp';
-                    break;
+            // Direct comparison checks.
+            case 'eq': // equals
+            case 'ne': // does not equal
+            case 'eqi': // equals (case insensitive)
+            case 'nei': // does not equal (case insensitive)
+                $method = 'criterionToDirect';
+                break;
 
-                // Wildcard comparison checks (contains/includes)
-                case 'inc': // includes
-                case 'ninc': // does not include
-                case 'inci': // includes (case insensitive)
-                case 'ninci': // does not include (case insensitive)
-                    $method = 'criterionToDQLInc';
-                    break;
+            // Relative comparison checks.
+            case 'gt': // greater than
+            case 'ge': // greater than or equal to
+            case 'lt': // less than
+            case 'le': // less than or equal to
+                $method = 'criterionToRelative';
+                break;
 
-                // Wildcard comparison checks (begins with)
-                case 'begins': // begins with
-                case 'nbegins': // does not begin with
-                case 'beginsi': // begins with (case insensitive)
-                case 'nbeginsi': // does not begin with (case insensitive)
-                    $method = 'criterionToDQLBegins';
-                    break;
+            // Wildcard comparison checks (contains/includes)
+            case 'inc': // includes
+            case 'ninc': // does not include
+            case 'inci': // includes (case insensitive)
+            case 'ninci': // does not include (case insensitive)
+                $method = 'criterionToContains';
+                break;
 
-                // Regex match
-                case 're': // matches regex string
-                    $method = 'criterionToDQLRe';
-                    break;
+            // Wildcard comparison checks (begins with)
+            case 'begins': // begins with
+            case 'nbegins': // does not begin with
+            case 'beginsi': // begins with (case insensitive)
+            case 'nbeginsi': // does not begin with (case insensitive)
+                $method = 'criterionToBegins';
+                break;
 
-                // Check for a list of values (match or no match).
-                case 'in': // is in the list
-                case 'nin': // is not in the list
-                case 'ini': // is in the list (case insensitive)
-                case 'nini': // is not in the list (case insensitive)
-                    $method = 'criterionToDQLIn';
-                    break;
-            }
+            // Regex match
+            case 're': // matches regex string
+                $method = 'criterionToRegex';
+                break;
 
-            // Methods modify the $clause object so no return value required.
-            // Note no default method - keep default return values if the
-            // method is invalid or unrecognised.
-            if ( method_exists( $this, $method ) )
-            {
-                $this->$method( $clause, $criterion );
-            }
+            // Check for a list of values (match or no match).
+            case 'in': // is in the list
+            case 'nin': // is not in the list
+            case 'ini': // is in the list (case insensitive)
+            case 'nini': // is not in the list (case insensitive)
+                $method = 'criterionToIn';
+                break;
+        }
+
+        return $method;
     }
 }
--- Ucc/Exception/Data/InvalidDataValueException.php
+++ Ucc/Exception/Data/InvalidDataValueException.php
@@ -5,6 +5,7 @@ namespace Ucc\Exception\Data;
 use Ucc\Exception\Data\InvalidDataException;
 
 /**
+ * Ucc\Exception\Data\InvalidDataException\InvalidDataValueException
  * This exception is thrown when a data value does not match requirements specified.
  *
  * @author Kris Rybak <kris@krisrybak.com>
