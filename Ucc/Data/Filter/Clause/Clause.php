e141e28a5784c5615d04ba3b409e5bf3b564c677
krisrybak <kris.rybak@hotmail.co.uk>
2015-04-17 09:14:39 +0100

    Adds Clause class

---
+++ Ucc/Data/Filter/Clause/Clause.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Ucc\Data\Filter\Clause;
+
+use \InvalidArgumentException;
+use Ucc\Data\Filter\Clause\ClauseInterface;
+
+/**
+ * Ucc\Data\Filter\Clause\Clause
+ * Allows to represent filter criteria in sting logic format
+ *
+ * @author Kris Rybak <kris@krisrybak.com>
+ */
+class Clause implements ClauseInterface
+{
+}
