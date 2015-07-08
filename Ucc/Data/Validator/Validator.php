04472f14d6dd0f3193890ed23cf85c254db2d3ef
krisrybak <kris.rybak@hotmail.co.uk>
2015-07-08 14:53:28 +0100

    Adds $checks

--- Ucc/Data/Validator/Validator.php
+++ Ucc/Data/Validator/Validator.php
@@ -1,3 +1,39 @@
 <?php
 
 namespace Ucc\Data\Validator;
+
+/**
+ * Ucc\Data\Validator\Validator
+ * Provides methods to validate data against defined checks
+ *
+ * @author Kris Rybak <kris@krisrybak.com>
+ */
+class Validator
+{
+    private $checks;
+
+    public function __construct()
+    {
+        $this->checks = array();
+    }
+
+    public function getChecks()
+    {
+        return $this->checks();
+    }
+
+    public function setChecks()
+    {
+        foreach ($checks as $key => $check) {
+            $this->setCheck($key, $check);
+        }
+    }
+
+    /**
+     * Checks inputs conformity with field checks set.
+     */
+    public function validate()
+    {
+
+    }
+}
