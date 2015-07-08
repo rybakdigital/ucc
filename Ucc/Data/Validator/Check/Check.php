4906518e076cb43d39a5ee16c754f0f24c534ed6
krisrybak <kris.rybak@hotmail.co.uk>
2015-07-08 18:47:58 +0100

    Adds class for a single Check

---
+++ Ucc/Data/Validator/Check/Check.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Ucc\Data\Validator\Check;
+
+/**
+ * Ucc\Data\Validator\Validator\Check\Check
+ * Provides methods and storage for a single check.
+ *
+ * @author Kris Rybak <kris@krisrybak.com>
+ */
+class Check
+{
+}
