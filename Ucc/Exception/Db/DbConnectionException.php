e213b7cd9423a3bf6024f405c175192a0390ea64
krisrybak <kris.rybak@hotmail.co.uk>
2015-12-03 06:49:15 +0000

    Adds DbConnectionException class

---
+++ Ucc/Exception/Db/DbConnectionException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ucc\Exception\Db;
+
+use \Exception;
+
+/**
+ * This exception is thrown when Db Connection experiance an error.
+ *
+ * @author Kris Rybak <kris@krisrybak.com>
+ */
+class DbConnectionException extends Exception
+{
+    
+}
