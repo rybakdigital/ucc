a46640b8a265f1a9eb82247a74bbe1bbef6a4479
Kris Rybak <kris.rybak@hotmail.co.uk>
2020-10-05 16:33:41 +0100

    Adds japanese numpbers

--- Ucc/Data/Types/Basic/PhoneNumberType.php
+++ Ucc/Data/Types/Basic/PhoneNumberType.php
@@ -133,6 +133,7 @@ class PhoneNumberType implements TypeInterface
             '91'    => '/^\+91[1-9][0-9]{9}$/',
             '90'    => '/^\+90[2-5][0-9]{9}$/',
             '880'   => '/^\+880[1-9][0-9]{9}$/',
+            '81'    => '/^\+81[1-9][0-9]{8,9}$/',
             '66'    => '/^\+66[2-9][0-9]{7,8}$/',
             '65'    => '/^\+65[1-9][0-9]{7}$/',
             '49'    => '/^\+49[1-9][0-9]{2,11}$/',
@@ -209,6 +210,9 @@ class PhoneNumberType implements TypeInterface
             '880'  => 'the 	Bangladeshi international numbering plan starts +880, followed'
                 . ' by the phone number with the leading 0 removed.'
                 . ' Length Min:13 Max:13',
+            '81'  => 'the Japanese international numbering plan starts +81, followed'
+                . ' by the phone number with the leading 0 removed.'
+                . ' Length Min:11 Max:12',
             '66'  => 'the Thai international numbering plan starts +66, followed'
                 . ' by a digit between 2 and 9.'
                 . ' Length Min:10 Max:11',
